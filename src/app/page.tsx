7b74d9f5f5c5fe9f73928e9ed05867e632f52781
Sami Männistö <sami.mannisto1@gmail.com>
2024-12-29 22:05:41 +0200

    Initialize project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="container">
+      <p>Hello world!</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        md: "1.5rem",
+        lg: "2rem"
+      }
+    }
+  },
+  plugins: []
+} satisfies Config;
